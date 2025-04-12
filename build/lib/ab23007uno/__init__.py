d6cb430f80166c4175eb0a96c4cbb9ae5710b29e
Katya <ab23007@ues.edu.sv>
2025-04-12 14:53:36 -0700

    Agregando cramer al init

--- build/lib/ab23007uno/__init__.py
+++ build/lib/ab23007uno/__init__.py
@@ -4,5 +4,4 @@ from .gauss_jordan import gauss_jordan
 from .gauss_seidel import gauss_seidel
 from .jacobi import jacobi
 from .lu_decomposition import lu_decomposition
-from .cramer_rule import cramer
 
